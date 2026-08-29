40abbdc2f78cd2de3646c4927ead9b9e9c249746
Juan Diego Garzon <juandiegar@gmail.com>
2026-08-29 11:15:09 -0500

    create aws infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,10 @@ terraform {
       source  = "hashicorp/null"
       version = "~>3.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.8.0"
+    }
   }
 
   backend "s3" {
@@ -21,4 +25,46 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "lambda_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "${path.module}/bootstrap"
+  output_path = "/tmp/function.zip"
+}
+
+resource "aws_lambda_function" "utils_function" {
+  function_name = "utils_lambda_function"
+  architectures = ["arm64"]
+  role          = aws_iam_role.example.arn
+  filename      = data.archive_file.zip.output_path
+  runtime       = "provided.al2023"
+  handler       = "main.handler"
+}
+
+resource "aws_lambda_function_url" "function_url" {
+  authorization_type = "NONE"
+  function_name      = aws_lambda_function.utils_function.function_name
+}
+
+output "function_url" {
+  value = aws_lambda_function_url.function_url.function_url
 }
