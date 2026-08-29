d8a3f925cea661532c22ca9bbfd822994ae29bcc
Juan Diego Garzon <juandiegar@gmail.com>
2026-08-29 15:20:04 -0500

    fix

--- src/main.go
+++ src/main.go
@@ -8,30 +8,30 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(r events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
 
 	type Person struct {
 		Name string `json:"name"`
 		Age  int    `json:"age"`
 	}
 
-	var response events.APIGatewayProxyResponse
+	var response events.APIGatewayV2HTTPResponse
 
-	switch r.Path {
+	switch r.RawPath {
 	case "/api/greet":
-		response = events.APIGatewayProxyResponse{StatusCode: 200, Body: "hello"}
-	case "api/data":
+		response = events.APIGatewayV2HTTPResponse{StatusCode: 200, Body: "hello"}
+	case "/api/data":
 		person := Person{Name: "Juan Diego", Age: 22}
 		encoded_json, err := json.Marshal(person)
 		if err != nil {
-			response = events.APIGatewayProxyResponse{StatusCode: 500}
+			response = events.APIGatewayV2HTTPResponse{StatusCode: 500}
 			break
 		}
-		response = events.APIGatewayProxyResponse{StatusCode: 200, Body: string(encoded_json)}
+		response = events.APIGatewayV2HTTPResponse{StatusCode: 200, Body: string(encoded_json)}
 	default:
-		response = events.APIGatewayProxyResponse{StatusCode: 404, Body: "Not found"}
+		response = events.APIGatewayV2HTTPResponse{StatusCode: 404, Body: "Not found"}
 	}
-	log.Println(r.Path)
+	log.Println(r.RawPath)
 	return response, nil
 }
 
