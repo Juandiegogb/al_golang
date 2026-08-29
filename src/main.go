0316d5c53a2d06d187b1014963844e5c25e7819e
Juan Diego Garzon <juandiegar@gmail.com>
2026-08-29 15:00:25 -0500

    testing feature

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -8,8 +9,30 @@ import (
 )
 
 func handler(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	type Person struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	var response events.APIGatewayProxyResponse
+
+	switch r.Path {
+	case "/api/greet":
+		response = events.APIGatewayProxyResponse{StatusCode: 200, Body: "hello"}
+	case "api/data":
+		person := Person{Name: "Juan Diego", Age: 22}
+		encoded_json, err := json.Marshal(person)
+		if err != nil {
+			response = events.APIGatewayProxyResponse{StatusCode: 500}
+			break
+		}
+		response = events.APIGatewayProxyResponse{StatusCode: 200, Body: string(encoded_json)}
+	default:
+		response = events.APIGatewayProxyResponse{StatusCode: 404, Body: "Not found"}
+	}
 	fmt.Println(r)
-	return events.APIGatewayProxyResponse{StatusCode: 200, Body: "hola como estas"}, nil
+	return response, nil
 }
 
 func main() {
